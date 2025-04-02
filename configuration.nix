6b9c4c741c754f809dd70e47847b9002d1e18f33
NixElf <agroxelf@gmail.com>
2025-04-02 00:14:15 -0700

    adding neovim

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
+    neovim
         (pkgs.writeShellScriptBin "nixsave" (builtins.readFile /etc/nixos/scripts/nixsave))
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
